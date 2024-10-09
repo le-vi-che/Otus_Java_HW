ed6ad03937f50e73a85bdbc8f9f63bb75104f042
e.emonova <lena.jemonova@gmail.com>
2024-10-09 20:46:42 +0300

    init

---
+++ src/main/java/otus/java/hw3/FirstHomeWork.java
@@ -0,0 +1,9 @@
+package otus.java.hw3;
+
+
+public class FirstHomeWork {
+    public static void main(String[] args) {
+    }
+}
+
+
---
+++ src/main/java/otus/java/hw5/FifthHomeWork.java
@@ -0,0 +1,11 @@
+package otus.java.hw5;
+
+
+public class FifthHomeWork {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
