6239d3fc660e064fcef579271ed68841663cd1e1
e.emonova <lena.jemonova@gmail.com>
2024-11-30 11:49:10 +0300

    исключения hw 15

---
+++ src/test/java/exeptions/AppArrayDataException.java
@@ -0,0 +1,7 @@
+package exeptions;
+
+public class AppArrayDataException extends Exception {
+    public AppArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/test/java/exeptions/AppArraySizeExeption.java
@@ -0,0 +1,7 @@
+package exeptions;
+
+public class AppArraySizeExeption extends Exception {
+    public AppArraySizeExeption(String message) {
+        super(message);
+    }
+}
