af20d43ecbd3f9304d15e1f1c3669b1dee002d87
e.emonova <lena.jemonova@gmail.com>
2025-01-25 15:31:28 +0300

    hw_29 fix

--- hw29/src/main/java/ru/otus/chat/Main.java
+++ hw29/src/main/java/ru/otus/chat/Main.java
@@ -2,26 +2,26 @@ package ru.otus.chat;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.InvalidPathException;
 import java.util.Scanner;
 
 public class Main {
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
 
-    System.out.print("Введите имя файла: ");
-    String fileName = scanner.nextLine();
+            System.out.print("Введите имя файла: ");
+            String fileName = scanner.nextLine();
 
-    System.out.print("Введите искомую последовательность символов: ");
-    String searchString = scanner.nextLine();
+            System.out.print("Введите искомую последовательность символов: ");
+            String searchString = scanner.nextLine();
 
-    System.out.println("Введенная последовательность встречается " + countSequence(fileName, searchString) + " раз");
+            System.out.println("Введенная последовательность встречается " + countSequence(fileName, searchString) + " раз");
+        }
+    }
 
-    scanner.close();
-}
-
-public static int countSequence(String fileName, String searchString) {
-    int count = 0;
-    if (searchString.isEmpty()) {
+    public static int countSequence(String fileName, String searchString) {
+        int count = 0;
+        if (searchString.trim().isEmpty()) {
             return 0;
         }
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName, StandardCharsets.UTF_8))) {
@@ -33,10 +33,9 @@ public static int countSequence(String fileName, String searchString) {
                     index += searchString.length(); // Перемещаем индекс вперед
                 }
             }
-        } catch (IOException e) {
-            System.err.println("Ошибка при чтении файла: " + e.getMessage());
+        } catch (IOException | InvalidPathException e) {
+            System.err.println("Ошибка при работе с файлом: " + e.getMessage());
         }
         return count;
     }
-
 }
